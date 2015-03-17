32da1a6a648833a0c1576800b63b88b92b526570
Royce Williams <royce@techsolvency.com>
2015-03-16 22:13:50 -0800

    Purge UUOCs

--- get-whois-ips.sh
+++ get-whois-ips.sh
@@ -11,11 +11,11 @@ ipsort () { sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n $*; }
 
 #-----------------------------------------------------------------------
 
-for host in `cat list-superset.txt  | awk '{print $2}' | sort -u`; do 
+for host in ` awk '{print $2}' whois.conf | sort -u`; do 
     host -t A -4 $host 2>&1 | grep 'has address'; 
 done | tee whois-ips-raw.txt
 
-cat whois-ips-raw.txt | awk '{print $4}' | sort -u | ipsort >whois-ips-whitelist.txt 
+awk '{print $4}' whois-ips-raw.txt| sort -u | ipsort >whois-ips-whitelist.txt 
 wc -l whois-ips-whitelist.txt
 
 #-----------------------------------------------------------------------
